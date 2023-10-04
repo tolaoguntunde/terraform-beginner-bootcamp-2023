ec5e52095fb298563d85b944de1e7d362aba7d21
tolaoguntunde <tolaoguntunde@gmail.com>
2023-10-04 14:29:02 -0400

    created modules

--- providers.tf
+++ providers.tf
@@ -1,22 +1,22 @@
-terraform {
-#   cloud {
-#     organization = "serah-terraform"
+# terraform {
+# #   cloud {
+# #     organization = "serah-terraform"
 
-#     workspaces {
-#       name = "terra-house-1"
+# #     workspaces {
+# #       name = "terra-house-1"
+# #     }
+# #   }
+#   required_providers {
+#     # random = {
+#     #   source = "hashicorp/random"
+#     #   version = "3.5.1"
+#     # }
+#     aws = {
+#       source = "hashicorp/aws"
+#       version = "5.18.1"
 #     }
 #   }
-  required_providers {
-    # random = {
-    #   source = "hashicorp/random"
-    #   version = "3.5.1"
-    # }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.18.1"
-    }
-  }
-}
+# }
 
 # provider "random" {
 #   # Configuration options
--- variables.tf
+++ variables.tf
@@ -1,20 +1,11 @@
 variable "user_uuid" {
-  description = "The UUID of the user."
+
   type        = string
-  # default     = "default-uuid" # You can set a default value if needed
   
-  validation {
-    condition = can(regex("^[a-f0-9]{8}-[a-f0-9]{4}-[4][a-f0-9]{3}-[89ab][a-f0-9]{3}-[a-f0-9]{12}$", var.user_uuid))
-    error_message = "Invalid UUID format. Must be in the format 'xxxxxxxx-xxxx-4xxx-9xxx-xxxxxxxxxxxx'."
-  }
 }
 
 variable "s3_bucket_name" {
-  description = "The name of the AWS S3 bucket."
+  
   type        = string
 
-  validation {
-    condition     = can(regex("^([a-z0-9.-]+)$", var.s3_bucket_name))
-    error_message = "Invalid S3 bucket name. It must be between 3 and 63 characters, contain only lowercase letters, numbers, hyphens, and periods, and start and end with a lowercase letter or number."
-  }
 }
