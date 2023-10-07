fae347f9ebc3458d7938d818fe4fd8de1c8964d6
tolaoguntunde <50242126+tolaoguntunde@users.noreply.github.com>
2023-10-07 10:45:04 -0400

    21 s3 static website hosting (#23)
    * static setup
    
    * applied changes specified in issues 21
    
    * update files

---
+++ modules/terrahouse_aws/variables.tf
@@ -0,0 +1,42 @@
+variable "user_uuid" {
+  description = "The UUID of the user."
+  type        = string
+  # default = "82ea8b84-b959-4500-8622-4fa974959cff"
+  
+  # default     = "default-uuid" # You can set a default value if needed
+  
+  # validation {
+  #   condition = can(regex("^[a-f0-9]{8}-[a-f0-9]{4}-[4][a-f0-9]{3}-[89ab][a-f0-9]{3}-[a-f0-9]{12}$", var.user_uuid))
+  #   error_message = "Invalid UUID format. Must be in the format 'xxxxxxxx-xxxx-4xxx-9xxx-xxxxxxxxxxxx'."
+  # }
+}
+
+variable "s3_bucket_name" {
+  description = "The name of the AWS S3 bucket."
+  type        = string
+  # default = "serahstatic"
+  # validation {
+  #   condition     = can(regex("^([a-z0-9.-]+)$", var.s3_bucket_name))
+  #   error_message = "Invalid S3 bucket name. It must be between 3 and 63 characters, contain only lowercase letters, numbers, hyphens, and periods, and start and end with a lowercase letter or number."
+  # }
+}
+
+variable "index_html_filepath" {
+  type        = string
+  description = "The file path to the index.html file"
+  # default     = "path/to/your/index.html"
+  validation {
+    condition     = can(file(var.index_html_filepath))
+    error_message = "The specified index.html file path does not exist."
+  }
+}
+
+variable "error_html_filepath" {
+  type        = string
+  description = "The file path to the index.html file"
+  # default     = "path/to/your/index.html"
+  validation {
+    condition     = can(file(var.error_html_filepath))
+    error_message = "The specified index.html file path does not exist."
+  }
+}
