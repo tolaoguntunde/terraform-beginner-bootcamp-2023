d4a8331b85bfa83e347c8b979c762cad9ea60767
tolaoguntunde <tolaoguntunde@gmail.com>
2023-10-07 19:29:43 -0400

    completed issue 28 task

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,7 @@ output "bucket_name" {
 output "website_endpoint" {
   value = module.terrahouse_aws.website_endpoint
 }
+
+output "cloudfront_url" {
+  value = module.terrahouse_aws.cloudfront_url
+}
