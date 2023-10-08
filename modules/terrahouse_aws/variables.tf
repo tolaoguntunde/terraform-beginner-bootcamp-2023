eb6b21bcbe0355be1da3890c4180630ce299d43c
tolaoguntunde <tolaoguntunde@gmail.com>
2023-10-07 20:18:15 -0400

    completed task

--- modules/terrahouse_aws/variables.tf
+++ modules/terrahouse_aws/variables.tf
@@ -51,3 +51,9 @@ variable "content_version" {
     error_message = "Content version must be a positive integer starting at 1."
   }
 }
+
+
+variable "assets_path" {
+  description = "Path to asset folder"
+  type = string
+}
