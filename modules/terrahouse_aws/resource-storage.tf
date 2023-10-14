176640de25431db8126e19bbee37c4b19a75416f
tolaoguntunde <tolaoguntunde@gmail.com>
2023-10-14 12:34:26 -0400

    terraform-cloud

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -27,9 +27,9 @@ resource "aws_s3_bucket_website_configuration" "website_configuration" {
 resource "aws_s3_object" "index_html" {
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "index.html"
-  source       = var.index_html_filepath
+  source       = "${path.root}${var.index_html_filepath}"
   content_type = "text/html"
-  etag = filemd5(var.index_html_filepath)
+  etag = filemd5("${path.root}${var.index_html_filepath}")
   lifecycle {
     replace_triggered_by = [ terraform_data.content_version.output ]
     ignore_changes = [etag]
@@ -41,9 +41,9 @@ resource "aws_s3_object" "index_html" {
 resource "aws_s3_object" "error_html" {
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "error.html"
-  source       = var.error_html_filepath
+  source       = "${path.root}${var.error_html_filepath}"
   content_type = "text/html"
-  etag = filemd5(var.error_html_filepath)
+  etag = filemd5("${path.root}${var.error_html_filepath}")
   lifecycle {
     ignore_changes = [etag]
   }
