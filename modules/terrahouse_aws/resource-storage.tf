e91e3e5a9c0d0c6fb37500a18bfbd96c277c2921
tolaoguntunde <tolaoguntunde@gmail.com>
2023-10-14 12:41:32 -0400

    terraform-cloud

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -83,9 +83,9 @@ resource "aws_s3_object" "upload_assets" {
   for_each = fileset("${path.root}/public/assets","*.{jpeg,png,gif}")
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "assets/${each.key}"
-  source = "${var.assets_path}/${each.key}"
+  source = "${path.root}${var.assets_path}/${each.key}"
   #content_type = "text/html"
-  etag = filemd5("${var.assets_path}/${each.key}")
+  etag = filemd5("${path.root}${var.assets_path}/${each.key}")
   lifecycle {
     replace_triggered_by = [ terraform_data.content_version.output ]
     ignore_changes = [etag]
